e7d1dfd6ae21064debd30b88fe973706e6034461
mark <maliqinag@yit.com>
2018-06-03 21:31:04 +0800

    添加list移除元素

--- src/main/java/com/mak/learn/GroupByTest.java
+++ src/main/java/com/mak/learn/GroupByTest.java
@@ -12,8 +12,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.stream.Collectors;
 
 /**
@@ -22,7 +24,7 @@ import java.util.stream.Collectors;
  * @description:对一个对象进行部分属性的筛选和部分属性分组的操作，最终组成一个hashmap
  * @date 2018/6/3 下午7:54
  */
-@Slf4j
+//@Slf4j
 public class GroupByTest {
 
     @Test
@@ -44,6 +46,39 @@ public class GroupByTest {
                 .collect(Collectors.groupingBy(User::getAge,
                         Collectors.mapping(User::getCoverUrl,Collectors.toList())));
 
-        log.info(JSON.toJSONString(result));
+//        log.info(JSON.toJSONString(result));
+    }
+
+    @Test
+    public void testRemoveValue(){
+        List<Integer> ids = new ArrayList<>();
+        CopyOnWriteArrayList<Integer> cowList = new CopyOnWriteArrayList<>(ids);
+
+        ids.add(11);
+        ids.add(22);
+        ids.add(33);
+
+        int[] excludeIds = new int[]{11,22};
+        //error
+//        for (Integer id : cowList) {
+//            for (int excludeId : excludeIds) {
+//                if (id.equals(excludeId)) {
+//                    cowList.remove(excludeId);
+//                }
+//            }
+//        }
+
+        //correct
+        Iterator<Integer> iter = ids.iterator();
+        while (iter.hasNext()) {
+            Integer item = iter.next();
+            for (int excludeId : excludeIds) {
+                if (item.equals(excludeId)) {
+                    iter.remove();
+                }
+            }
+        }
+
+        System.out.println(JSON.toJSONString(ids));
     }
 }
